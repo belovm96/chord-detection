3f11bc69b12b49981f36e53ca0f48706f7a21174
Mike Belov <47603679+belovm96@users.noreply.github.com>
2020-09-22 14:51:11 -0400

    wrote setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="chord-detection",
+    author="Mike Belov",
+    author_email="belovmikhail96@gmail.com",
+    url="https://github.com/belovm96/chord-recognition",
+    packages=["ChordDetection"],
+)

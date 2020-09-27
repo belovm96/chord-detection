3d5b1c3c24fead5a9321c8cfadedae0b6466d5c8
Mike Belov <47603679+belovm96@users.noreply.github.com>
2020-09-27 19:19:46 -0400

    fixed train generator

--- ChordDetection/modeling/iterators.py
+++ ChordDetection/modeling/iterators.py
@@ -35,32 +35,27 @@ class PreprocessFeatures:
             
             np.save(self.dir_aligned+folder+'/'+files[0], features)
             np.save(self.dir_aligned+folder+'/'+files[1], targets)
-            
-        
-cur_dir = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/mcgill-billboard-prep/'
-aligned_dir = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/mcgill-billboard-aligned/'
 
 
 class Batch:
-    def __init__(self, batch_size, context_size, path_to_train, path_to_test, path_to_val, augment=True, randomise=False):
+    def __init__(self, batch_size, context_size, path_to_train, path_to_test, path_to_val, augment=True, randomise=True):
         self.batch_size = batch_size
         self.context_size = context_size
         self.augment = augment
         if self.augment:
-            self.shift_sem = SemitoneShift(0.3, 1, 2)
+            self.shift_sem = SemitoneShift(0.3, 2, 2)
             self.detune = Detuning(0.3, 0.4, 2)
-
         self.randomise = randomise
         self.path_to_train = path_to_train
         self.path_to_val = path_to_val
         self.path_to_test = path_to_test
 
-        
+    
     def _generator(self, split):
         if split == 'train':
             b_size = self.batch_size
             self.path_to_data = self.path_to_train
-        if split == 'val':
+        elif split == 'val':
             self.path_to_data = self.path_to_val
             b_size = 1
         else:
@@ -70,46 +65,7 @@ class Batch:
         num_frames = 2 * self.context_size + 1
 
         batch = []
-
-        while True:
-            folders = os.listdir(self.path_to_data)
-            if self.randomise and split == 'train':
-                random.shuffle(folders)
-            for folder in folders:
-                files = os.listdir(self.path_to_data+folder)
-                spec = np.load(self.path_to_data+folder+'/'+files[0])
-                target = np.load(self.path_to_data+folder+'/'+files[1])
-                if self.context_size:
-                    for i in range(spec.shape[0] - num_frames):
-                        spect = spec[i:num_frames+i, :]
-                        spect_scaled = preprocessing.scale(spect)
-                        targ = target[i+self.context_size, :]
-                        batch.append((spect_scaled, targ))
-                        b_size -= 1
-                        if b_size == 0:
-                            yield batch
-                            if split == 'train':
-                                b_size = self.batch_size
-                            else:
-                                b_size = 1
-                            batch = []
-
-
-    def _generator_test(self, split):
-        if split == 'train':
-            b_size = self.batch_size
-            self.path_to_data = self.path_to_train
-        if split == 'val':
-            self.path_to_data = self.path_to_val
-            b_size = 1
-        else:
-            self.path_to_data = self.path_to_test
-            b_size = 1
-
-        num_frames = 2 * self.context_size + 1
-
-        batch = []
-
+        
         while True:
             folders = os.listdir(self.path_to_data)
             if self.randomise and split == 'train':
@@ -141,17 +97,25 @@ class Batch:
                 if rand_int == 0:
                     batch = self.detune(batch, self.batch_size)
                 else:
-                    continue
                     batch = self.shift_sem(batch, self.batch_size)
-                    
-            data_batch = np.array(batch[0])
-            targets = np.array(batch[1])
-            
-            data_batch = np.reshape(data_batch, (self.batch_size, 1, data_batch.shape[1], data_batch.shape[2]))
+
+                data_batch = np.array(batch[0])
+                targets = np.array(batch[1])
+                data_batch = np.reshape(data_batch, (self.batch_size, 1, data_batch.shape[1], data_batch.shape[2]))
+
+            else:
+                data_batch = np.zeros((self.batch_size, 1, 105, 15))
+                targets = np.zeros((self.batch_size, 25))
+                i = 0
+                for data, target in batch:
+                    data = data.transpose()
+                    data_batch[i, 0, :, :] = data
+                    targets[i, :] = target
+                    i += 1
             yield data_batch, targets
 
     def val_generator(self):
-        for batch in self._generator_test('val'):
+        for batch in self._generator('val'):
             batch_scaled = np.zeros((1, 1, batch[0][0].shape[1], batch[0][0].shape[0]))
             targets = np.zeros((1, batch[0][1].shape[0]))
             ex_prep = batch[0][0].transpose()
@@ -160,7 +124,7 @@ class Batch:
             yield batch_scaled, targets
     
     def test_generator(self):
-       for batch in self._generator_test('test'):
+       for batch in self._generator('test'):
             batch_data = batch[0][0]
             targets = batch[0][1]
             batch_scaled = preprocessing.scale(batch_data)
@@ -168,11 +132,3 @@ class Batch:
             batch_scaled = batch_scaled.reshape(1, 1, batch_scaled.shape[0], batch_scaled.shape[1])
             targets = targets.reshape(1, targets.shape[0])
             yield batch_scaled, targets
-    
-
-                    
-
-
-        
-                
-        

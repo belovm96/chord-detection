d6f66a48eb5e43a1883540933b4f77018ffd553b
Mike Belov <47603679+belovm96@users.noreply.github.com>
2020-09-27 19:17:23 -0400

    changed epoch num

--- ChordDetection/modeling/cnn.py
+++ ChordDetection/modeling/cnn.py
@@ -12,39 +12,39 @@ from iterators import Batch
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(1, 105, 15), padding='same', data_format='channels_first'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.3))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15),  padding='same', data_format='channels_first'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.3))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15), padding='same', data_format='channels_first'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.3))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15),  padding='same', data_format='channels_first'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.3))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.MaxPooling2D((2, 1), data_format='channels_first'))
 
 model.add(layers.Conv2D(64, 3, activation='relu', input_shape=(32, 52, 15), padding='valid', data_format='channels_first'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.3))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.Conv2D(64, 3, activation='relu', input_shape=(64, 50, 13),  padding='valid', data_format='channels_first'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.3))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.MaxPooling2D((2, 1), data_format='channels_first'))
 
 model.add(layers.Conv2D(128, (12, 9), activation='relu', input_shape=(64, 24, 11), padding='valid', data_format='channels_first'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.3))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.Conv2D(25, (1, 1), activation='linear', input_shape=(128, 13, 3), padding='valid', data_format='channels_first'))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dropout(0.3))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.AveragePooling2D((13, 3), data_format='channels_first'))
@@ -57,43 +57,45 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 
-checkpoint_dir = '/home/ubuntu/chord-detection/ChordDetection/modeling/checkpoint'
-EPOCHS = 20
+checkpoint_dir = '/home/ubuntu/chord-detection/ChordDetection/modeling/checkpoint_1'
+EPOCHS = 40
 BATCH_SIZE = 500
 CONTEXT_W = 7
 TOTAL_FRAMES = 720000
 
 callbacks = [
         EarlyStopping(patience=5, verbose=1),
-        ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
+        ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.00001, verbose=1),
         ModelCheckpoint(
             filepath=checkpoint_dir,
             verbose=1,
             save_best_only=True,
             save_weights_only=True
             ),
-        CSVLogger('training.log')
+        CSVLogger('training_1.log')
         ]
 
 
 path_to_train = '/home/ubuntu/mcgill-billboard-train/'
 path_to_val = '/home/ubuntu/mcgill-billboard-val/'
 path_to_test = '/home/ubuntu/mcgill-billboard-test/'
-batch_obj_1 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val)
-batch_obj_2 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val)
+
+batch_obj_1 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val, False, True)
+batch_obj_2 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val, False, True)
 training_generator = batch_obj_1.train_generator()
 validation_generator = batch_obj_2.val_generator()
 
 steps_per_epoch = TOTAL_FRAMES // BATCH_SIZE
 
+#model.load_weights(checkpoint_dir)
+
 model.fit(
         training_generator,
         validation_data=validation_generator,
-        validation_steps=70000,
+        validation_steps=100000,
         callbacks=callbacks,
         steps_per_epoch=steps_per_epoch,
         epochs=EPOCHS, verbose=True, 
         )
 
-model.save('/home/ubuntu/chord-detection/ChordDetection/modeling/')
-
+model.save('/home/ubuntu/chord-detection/ChordDetection/modeling/models')

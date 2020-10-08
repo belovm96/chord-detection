009a5b88e53cd8d4a9824d7703e58023f9384c8b
Mike Belov <47603679+belovm96@users.noreply.github.com>
2020-10-08 11:36:38 -0400

    added kernerl regs and reduced dropout

--- ChordDetection/modeling/cnn.py
+++ ChordDetection/modeling/cnn.py
@@ -9,42 +9,39 @@ from iterators import Batch
 import warnings
 warnings.filterwarnings('ignore')
 
-
 model = models.Sequential()
-model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(1, 105, 15), padding='same', data_format='channels_first'))
-model.add(layers.Dropout(0.3))
+
+reg = tf.keras.regularizers.L2(1e-5)
+model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(1, 105, 15), padding='same', data_format='channels_first', kernel_regularizer=reg))
 model.add(layers.BatchNormalization(axis=1))
 
-model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15),  padding='same', data_format='channels_first'))
-model.add(layers.Dropout(0.3))
+model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15),  padding='same', data_format='channels_first', kernel_regularizer=reg))
 model.add(layers.BatchNormalization(axis=1))
 
-model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15), padding='same', data_format='channels_first'))
-model.add(layers.Dropout(0.3))
+model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15), padding='same', data_format='channels_first', kernel_regularizer=reg))
 model.add(layers.BatchNormalization(axis=1))
 
-model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15),  padding='same', data_format='channels_first'))
-model.add(layers.Dropout(0.3))
-model.add(layers.BatchNormalization(axis=1))
-
-model.add(layers.MaxPooling2D((2, 1), data_format='channels_first'))
-
-model.add(layers.Conv2D(64, 3, activation='relu', input_shape=(32, 52, 15), padding='valid', data_format='channels_first'))
-model.add(layers.Dropout(0.3))
-model.add(layers.BatchNormalization(axis=1))
-
-model.add(layers.Conv2D(64, 3, activation='relu', input_shape=(64, 50, 13),  padding='valid', data_format='channels_first'))
-model.add(layers.Dropout(0.3))
+model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(32, 105, 15),  padding='same', data_format='channels_first', kernel_regularizer=reg))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.MaxPooling2D((2, 1), data_format='channels_first'))
+model.add(layers.Dropout(0.5))
 
-model.add(layers.Conv2D(128, (12, 9), activation='relu', input_shape=(64, 24, 11), padding='valid', data_format='channels_first'))
-model.add(layers.Dropout(0.3))
+model.add(layers.Conv2D(64, 3, activation='relu', input_shape=(32, 52, 15), padding='valid', data_format='channels_first', kernel_regularizer=reg))
 model.add(layers.BatchNormalization(axis=1))
 
-model.add(layers.Conv2D(25, (1, 1), activation='linear', input_shape=(128, 13, 3), padding='valid', data_format='channels_first'))
-model.add(layers.Dropout(0.3))
+model.add(layers.Conv2D(64, 3, activation='relu', input_shape=(64, 50, 13),  padding='valid', data_format='channels_first', kernel_regularizer=reg))
+model.add(layers.BatchNormalization(axis=1))
+
+model.add(layers.MaxPooling2D((2, 1), data_format='channels_first'))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Conv2D(128, (12, 9), activation='relu', input_shape=(64, 24, 11), padding='valid', data_format='channels_first', kernel_regularizer=reg))
+model.add(layers.BatchNormalization(axis=1))
+model.add(layers.Dropout(0.5))
+
+
+model.add(layers.Conv2D(25, (1, 1), activation='linear', input_shape=(128, 13, 3), padding='valid', data_format='channels_first', kernel_regularizer=reg))
 model.add(layers.BatchNormalization(axis=1))
 
 model.add(layers.AveragePooling2D((13, 3), data_format='channels_first'))
@@ -56,14 +53,13 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 
-checkpoint_dir = '/home/ubuntu/chord-detection/ChordDetection/modeling/models/model_exp_6/model_01_drop_0001_lr'
+checkpoint_dir = '/home/ubuntu/rechord/chord-detection/ChordDetection/modeling/models/cnn/exp_2/model_02'
 EPOCHS = 50
 BATCH_SIZE = 500
 CONTEXT_W = 7
-TOTAL_FRAMES = 800000
+TOTAL_FRAMES = 1400000
 
 callbacks = [
-        EarlyStopping(patience=5, verbose=1),
         ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
         ModelCheckpoint(
             filepath=checkpoint_dir,
@@ -71,16 +67,16 @@ callbacks = [
             save_best_only=True,
             save_weights_only=True
             ),
-        CSVLogger('/home/ubuntu/chord-detection/ChordDetection/modeling/models/model_exp_6/training.log')
+        CSVLogger('/home/ubuntu/rechord/chord-detection/ChordDetection/modeling/models/cnn/exp_2/training.log')
         ]
 
 
-path_to_train = '/home/ubuntu/mcgill-billboard-train/'
-path_to_val = '/home/ubuntu/mcgill-billboard-val/'
-path_to_test = '/home/ubuntu/mcgill-billboard-test/'
+path_to_train = '/home/ubuntu/data_train/'
+path_to_val = '/home/ubuntu/data_val/'
+path_to_test = '/home/ubuntu/data_test/'
 
-batch_obj_1 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val, False, True)
-batch_obj_2 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val, False, True)
+batch_obj_1 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val, True, True)
+batch_obj_2 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val, True, True)
 training_generator = batch_obj_1.train_generator()
 validation_generator = batch_obj_2.val_generator()
 
@@ -89,10 +85,8 @@ steps_per_epoch = TOTAL_FRAMES // BATCH_SIZE
 model.fit(
         training_generator,
         validation_data=validation_generator,
-        validation_steps=80000,
+        validation_steps=140000,
         callbacks=callbacks,
         steps_per_epoch=steps_per_epoch,
         epochs=EPOCHS, verbose=True, 
         )
-
-model.save('/home/ubuntu/chord-detection/ChordDetection/modeling/models/model_exp_6/model_01_drop_0001_lr.h5')

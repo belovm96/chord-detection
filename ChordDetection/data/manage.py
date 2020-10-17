4ee9569bef99cf9719b2864290774f6debbe3c09
Mike Belov <47603679+belovm96@users.noreply.github.com>
2020-10-17 18:38:07 -0400

    added argparse and changed func name

--- ChordDetection/data/manage.py
+++ ChordDetection/data/manage.py
@@ -1,10 +1,10 @@
 """
-Audio data management script
-
+Data Format Conversion and Annotation Matching Script
 @belovm96
 """
 import os
 import shutil
+import argparse
 
 class DataWrangling:
      def __init__(self, path_rename, path_renamed, path_convert_to, path_convert_from):
@@ -12,78 +12,69 @@ class DataWrangling:
          self.path_rename = path_rename
          self.path_convert_to = path_convert_to
          self.path_convert_from = path_convert_from
+         self.name_to_path = {}
     
-     def audio_rename(self, path_to_file, to_save_path):
-        path = path_to_file.split('/')
-        file_name = path[-1]
-        art_song = file_name.split('-')
+     def audio_rename(self):
+        for song in os.listdir(self.path_rename):
+            path = self.path_rename+'/'+song.split('/')
+            file_name = path[-1]
+            art_song = file_name.split('-')
+            
+            artist, title = art_song[0], art_song[1:]
+            
+            str_art = [char if char != ' ' and char != '&' else '_' for char in artist]
+            str_tit = [char if char != ' ' and char != '&' else '_' for word in title for char in word]
+            art_tit = str_art+str_tit
+            
+            song_name = ''.join(art_tit)
+            shutil.copy(self.path_rename+'/'+song, self.renamed+'/'+song_name)
         
-        artist, title = art_song[0], art_song[1:]
+     def mp3_to_wav(self):
+        for song in os.listdir(self.path_convert_from):
+            path = self.path_convert_from+'/'+song.split('/')
+            file_name = path[-1].strip()
+            wav_name = file_name[:-4]+'.wav'
+            os.system(f'ffmpeg -i {self.path_convert_from}/song {self.path_convert_to}/{wav_name}')
         
-        str_art = [char if char != ' ' and char != '&' else '_' for char in artist]
-        str_tit = [char if char != ' ' and char != '&' else '_' for word in title for char in word]
-        art_tit = str_art+str_tit
-        
-        song_name = ''.join(art_tit)
-        new_file_name_path = to_save_path+'/'+song_name
-        shutil.copy(path_to_file, new_file_name_path)
-        
-     def mp3_to_wav(self, mp3_path, to_save_path):
-        path = mp3_path.split('/')
-        file_name = path[-1].strip()
-        wav_name = file_name[:-4]+'.wav'
-        os.system(f'ffmpeg -i {mp3_path} {to_save_path}/{wav_name}')
-        
-     def manage_anns(self, songs_dir, song_list, save_songs_path, anns):
-        anns_path = open(anns, 'r')
-        anns_path = anns_path.readlines()
-        
-        song_list = open(song_list, 'r')
-        for c, song in enumerate(song_list.readlines()):
-            art_title = song[:-1]
-            art_title = art_title.strip()
+     def store(self, ann_paths, anns, save_files_to):
+        anns = open(anns, 'r')
+        ann_paths = open(ann_paths, 'r')
+        ann_paths = ann_paths.readlines()
+        for i, ann in enumerate(anns.readlines()):
+            ann = ann[:-1].strip()
             max_match = float('-inf')
             max_match_song =''
-            for song in songs_dir:
+            for song in self.name_to_path:
                 num_match = 0
-                for i, char in enumerate(song):
-                    if i < len(art_title) and art_title[i] == char:
+                for j, char in enumerate(song):
+                    if j < len(ann) and ann[j] == char:
                         num_match += 1   
                     if num_match > max_match:
                         max_match = num_match
                         max_match_song = song
-                        
-            if max_match >= len(art_title) - 5:
-                print(art_title)
-                os.mkdir(save_songs_path+art_title)
-                shutil.copy(songs_dir[max_match_song], save_songs_path+art_title)
-                shutil.copy(anns_path[c][:-1], save_songs_path+art_title)
-
-path_rename = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/rw-mp3'
-path_renamed = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/rw-mp3-renamed'
-path_convert_from = path_renamed
-path_convert_to = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/rw-wav-renamed'
-
-dr_obj = DataWrangling(path_rename, path_renamed, path_convert_to, path_convert_from)
-
-for song in os.listdir(path_rename):
-    dr_obj.audio_rename(path_rename+'/'+song, path_renamed)
-
-for song in os.listdir(path_convert_from):
-    dr_obj.mp3_to_wav(path_convert_from+'/'+song, path_convert_to)
-
-path_convert_to = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/dataset/audio_data/more-data-wav-renamed'
-songs_prep = {}
-for i, song in enumerate(os.listdir(path_convert_to)):
-    song_sliced = song.split('_')
-    song_sliced = [word for word in song_sliced if word != '']
-    song_sliced = ' '.join(song_sliced).lower().replace('\'','')
-    song_sliced = song_sliced[:-4]
-    songs_prep[song_sliced] = path_convert_to+'/'+song
-
-song_anns_path = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/songs.txt'
-save_songs_path = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/dataset/preprocessed/'
-anns = 'C:/Users/Mikhail/OneDrive/Desktop/chord-recognition/songs_path.txt'
-dr_obj.manage_anns(songs_prep, song_anns_path, save_songs_path, anns)
+        
+            if max_match >= len(ann) - 5:
+                if not os.path.isfile(save_files_to+'/'+ann):
+                    os.mkdir(save_files_to+'/'+ann)
+                    shutil.copy(self.name_to_path[max_match_song], save_files_to+'/'+ann)
+                    shutil.copy(ann_paths[i][:-1], save_files_to+'/'+ann)
+                
+                
+     def song_to_path(self):
+        for i, song in enumerate(os.listdir(self.path_convert_to)):
+            song_sliced = song.split('_')
+            song_sliced = [word for word in song_sliced if word != '' ]
+            song_sliced = ' '.join(song_sliced)[:-4].lower().replace('\'','')
+            self.name_to_path[song_sliced] = self.path_convert_to+'/'+song
 
 
+parser = argparse.ArgumentParser(description = "Script for managing audio files and chord annotations")
+parser.add_argument("--mp3", type=str, help="path to mp3 audio files")
+parser.add_argument("--mp3_renamed", type=str, help='path to store renamed mp3 files') 
+parser.add_argument("--wav_renamed", type=str, help='path to store renamed wav files')
+parser.add_argument("--ann_paths", type=str, help='file containing paths to annotations')
+parser.add_argument("--anns", type=str, help='path to file containing song names (artist title)')
+parser.add_argument("--save_to", type=str, help='path to folder to save annotations and songs')
+args = parser.parse_args()
+
+D_Wr = DataWrangling(args.mp3, args.mp3_renamed, args.wav_renamed, args.mp3_renamed)

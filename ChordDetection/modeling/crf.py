0ee6f6a5b72f045a534dc36e956238851403c067
Mike Belov <47603679+belovm96@users.noreply.github.com>
2020-10-18 16:47:34 -0400

    added OOP

--- ChordDetection/modeling/crf.py
+++ ChordDetection/modeling/crf.py
@@ -1,64 +1,119 @@
+"""
+CRF model for chord sequence decoding
+@belom96
+"""
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 import tensorflow as tf
 from tf2crf import CRF
 from iterators import Batch
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger
+import argparse
+import yaml
 import warnings
 warnings.filterwarnings('ignore')
 
-SEQ_FRAMES = 1024
-FEATURE_DIM = 128
-EPOCHS = 20
-BATCH_SIZE = 20
-NUM_CLASSES = 25
-CONTEXT_W = 7
-TOTAL_EX = 400
+class SeqCRF:
+        def __init__(self, batch_size, context_w, L2, epochs, train_num, val_num, test_num,
+                train_path, val_path, test_path, save_weights, save_log, lr, lr_step, lr_final, patience, 
+        seq_dim, feat_dim, num_classes, weights=None, augment=True, randomise=True):
+                self.batch_s = batch_size
+                self.context_w = context_w
+                self.L2 = L2
+                self.epochs = epochs
+                self.train_num = train_num
+                self.val_num = val_num
+                self.test_num = test_num
+                self.train_path = train_path
+                self.val_path = val_path
+                self.test_path = test_path
+                self.save_weights = save_weights
+                self.save_log = save_log
+                self.lr = lr
+                self.lr_step = lr_step
+                self.lr_final = lr_final
+                self.patience = patience
+                self.augment = augment
+                self.randomise = randomise
+                self.steps_per_epoch = train_num // batch_size
+                self.weights = weights
+                self.num_classes = num_classes
+                self.seq_dim = seq_dim
+                self.feat_dim = feat_dim
+                self.num_classes = num_classes
+        
+        def __call__(self):
+                self.reg = tf.keras.regularizers.L2(self.L2)
+                input = Input(shape=(self.seq_dim, self.feat_dim), dtype='float32')
+                mid = Dense(self.num_classes, input_shape=(self.seq_dim, self.feat_dim), activation='softmax', kernel_regularizer=self.reg)(input)
+                self.crf = CRF(dtype='float32', sparse_target=True)
+                self.crf.sequence_lengths = self.seq_dim
+                self.crf.output_dim = self.num_classes
+                output = self.crf(mid)
+                model = Model(input, output)
+                
+                self.model = model
 
-input = Input(shape=(SEQ_FRAMES, FEATURE_DIM), dtype='float32')
-mid = Dense(NUM_CLASSES, input_shape=(SEQ_FRAMES, FEATURE_DIM), activation='linear')(input)
-crf = CRF(dtype='float32', sparse_target=True)
-crf.sequence_lengths = SEQ_FRAMES
-crf.output_dim = NUM_CLASSES
-output = crf(mid)
-model = Model(input, output)
+                return model
 
-opt = tf.keras.optimizers.Adam(learning_rate=0.01)
-model.compile(loss=crf.loss, optimizer=opt, metrics=[crf.accuracy])
+        def compile(self):
+                self.opt = tf.keras.optimizers.Adam(learning_rate=self.lr)
+                self.model.compile(loss=self.crf.loss, optimizer=self.opt, metrics=[self.crf.accuracy])
+        
+        def summary(self):
+                print(self.model.summary())
 
-path_to_train = '/home/ubuntu/mcgill-billboard-train/'
-path_to_val = '/home/ubuntu/mcgill-billboard-val/'
-path_to_test = '/home/ubuntu/mcgill-billboard-test/'
+        def callbacks(self):
+                self.callbacks = [
+                        ReduceLROnPlateau(factor=self.lr_step, patience=self.patience, min_lr=self.lr_final, verbose=1),
+                        ModelCheckpoint(
+                        filepath=self.save_weights,
+                        verbose=1,
+                        save_best_only=True,
+                        save_weights_only=True,
+                        monitor='val_accuracy'),
+                        CSVLogger(self.save_log)
+                        ]
+                
+        def build_generators(self):
+                batch_obj_1 = Batch(self.batch_s, self.context_w, self.train_path, self.test_path, self.val_path, self.augment, self.randomise)
+                batch_obj_2 = Batch(self.batch_s, self.context_w, self.train_path, self.test_path, self.val_path, self.augment, self.randomise)
+                batch_obj_3 = Batch(self.batch_s, self.context_w, self.train_path, self.test_path, self.val_path, self.augment, self.randomise)
 
-batch_obj_1 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val, False, True)
-batch_obj_2 = Batch(BATCH_SIZE, CONTEXT_W, path_to_train, path_to_test, path_to_val, False, True)
-training_generator = batch_obj_1.train_generator_seq()
-validation_generator = batch_obj_2.val_generator_seq()
+                self.training_generator = batch_obj_1.train_generator_seq()
+                self.validation_generator = batch_obj_2.val_generator_seq()
+                self.test_generator = batch_obj_3.test_generator_seq()
 
-checkpoint_dir = '/home/ubuntu/chord-detection/ChordDetection/modeling/models/crf_1'
+        def fit_model(self):
+                self.model.fit(
+                        self.training_generator,
+                        validation_data=self.validation_generator,
+                        validation_steps=self.val_num,
+                        callbacks=self.callbacks,
+                        steps_per_epoch=self.steps_per_epoch,
+                        epochs=self.epochs, verbose=True, 
+                        )
+        def test_model(self):
+                self.model.load_weights(self.weights)
+                history = self.model.evaluate(self.test_generator, verbose=1, steps=self.test_num)
+                print(history)
 
-callbacks = [
-        EarlyStopping(patience=5, verbose=1, monitor='val_loss'),
-        ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1, monitor='val_loss'),
-        ModelCheckpoint(
-            filepath=checkpoint_dir,
-            verbose=1,
-            save_best_only=True,
-            save_weights_only=True, 
-            monitor='val_accuracy'
-            ),
-        CSVLogger('/home/ubuntu/chord-detection/ChordDetection/modeling/training_crf.log')
-        ]
+parser = argparse.ArgumentParser(description = "Script for training and testing of the CNN")
+parser.add_argument("--path_config", type=str, help="path to config file")
+args = parser.parse_args()
 
-steps_per_epoch = TOTAL_EX // BATCH_SIZE
+with open(args.path_config) as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+    CRF_seq = SeqCRF(data['batch_size'], data['context_w'], data['reg']['L2'], data['epochs'], data['train_size'], data['val_size'], data['test_size'],
+                data['train_path'], data['val_path'], data['test_path'], data['save_best_weights'], data['save_log'], data['lr_scheduler']['init'],
+        data['lr_scheduler']['step'], data['lr_scheduler']['final'], data['lr_scheduler']['patience'], data['seq_frames'], data['feature_dim'], data['num_classes'] )
 
-model.fit(
-        training_generator,
-        validation_data=validation_generator,
-        validation_steps=60,
-        callbacks=callbacks,
-        steps_per_epoch=steps_per_epoch,
-        epochs=EPOCHS, verbose=True, 
-        )
+model = CRF_seq()
 
-model.save_weights('crf_model')
+CRF_seq.compile()
+CRF_seq.summary()
+
+CRF_seq.callbacks()
+CRF_seq.build_generators()
+
+CRF_seq.fit_model()

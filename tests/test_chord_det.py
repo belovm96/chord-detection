601e4d58fd1f980bb461f8097e485ab59deb9a39
Mike Belov <47603679+belovm96@users.noreply.github.com>
2020-10-25 18:18:55 -0400

    added prep tests

--- tests/test_chord_det.py
+++ tests/test_chord_det.py
@@ -3,6 +3,9 @@ import numpy.testing as npt
 
 import ChordDetection
 
+"""
+Sanity Checks
+"""
 def test_smoke_Chord_Dec():
     obj = ChordDetection.ChordDetectionObject()
 
@@ -10,3 +13,29 @@ def test_Chord_Dec_fizz():
     obj = ChordDetection.ChordDetectionObject()
     output = obj.fizz()
     npt.assert_equal(output, "buzz")
+
+"""
+Preprocessing Tests
+"""
+def test_feature_shape():
+    song_path = './data/U2 - With Or Without You - Remastered.wav'
+    LG_SPEC = ChordDetection.LogFiltSpec(8192, 24, 65, 2100, 10, True)
+    spec = LG_SPEC(song_path)
+    expected_shape = (3067, 105)
+    npt.assert_equal(np.array(spec).shape, expected_shape)
+
+def test_annotation_shape():
+    ann_path = './data/03-With_or_Without_You.lab'
+    ANN = ChordDetection.ChordsMajMin(10)
+    ann = ANN(ann_path)
+    expected_shape = (2960, 25)
+    npt.assert_equal(ann.shape, expected_shape)
+
+def test_alignment():
+    path = './data'
+    PREP = ChordDetection.PreprocessFeatures(path, path)
+    spec_shape, target_shape = PREP.align()
+    target_expected = (2960, 25)
+    spec_expected = (2960, 105)
+    npt.assert_equal(spec_shape, spec_expected)
+    npt.assert_equal(target_shape, target_expected)
